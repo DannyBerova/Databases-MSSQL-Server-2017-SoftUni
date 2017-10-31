f39420889f838e63eda17b9ef31d9316eec78de3
Danny Berova <danny.jordanova@gmail.com>
2017-10-31 16:05:54 +0200

    OOP-intro Inheritance

---
+++ DB-Advanced/OOP-intro-Inheritance/Inheritance/02.BookShop/GoldenEditionBook.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class GoldenEditionBook : Book
+{
+    public GoldenEditionBook( string author, string title, decimal price)
+    : base(author, title, price)
+    {
+        this.Price *= 1.3m;
+    }
+
+}
+
